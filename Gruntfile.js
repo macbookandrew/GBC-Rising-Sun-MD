ae1c13b7f8ad1fbf22e97145b063fea401cceb02
Andrew Minion <andrew@andrewrminion.com>
2015-11-19 09:54:08 -0500

    set up tooling

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    // Watch task config
+    watch: {
+        styles: {
+            files: "SCSS/*.scss",
+            tasks: ['sass', 'postcss'],
+        },
+    },
+    sass: {
+        dist: {
+            files: {
+                "style.min.css" : "style.scss"
+            }
+        }
+    },
+    postcss: {
+        options: {
+            map: {
+                inline: false
+            },
+            processors: [
+                require('pixrem')(), // add fallbacks for rem units
+                require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+                require('cssnano')() // minify the result
+            ]
+        },
+        dist: {
+            src: 'style.min.css',
+        }
+    },
+  });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', 'watch');
+};
